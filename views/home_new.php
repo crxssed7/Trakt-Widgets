758f46feb92c8473cdc42e0075c4291558ece6fa
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-04-28 20:12:48 +0100

    Update home_new.php

--- views/home_new.php
+++ views/home_new.php
@@ -1,39 +1,36 @@
 <?php
 
 $quotes = array (
-    "Ooohhhhhhhhh!!!!!^Regular Show",
-    "I'm Pickle Rickkk!!!!^Rick, Rick and Morty",
-    "Bazinga!^Sheldon Cooper, The Big Bang Theory",
-    "I am the danger, I am the one who knocks.^Walter White, Breaking Bad",
-    "Nobody exists on purpose.^Morty, Rick and Morty",
-    "Bird is the word.^Peter Griffin, Family Guy",
-    "You shall not pass!^The Lord of the Rings",
-    "Wubalubadubdub!^Rick, Rick and Morty",
-    "D'oh!^Homer Simpson, The Simpsons",
-    "Cool cool cool cool cool cool cool cool.^Jake Peralta, Brooklyn Nine-Nine",
-    "We're all goofy goobers.^Spongebob, Spongebob Square Pants",
-    "That's what she said.^Michael Scott, The Office (US)",
-    "Oh my God, they killed Kenny!^South Park",
-    "Oh my God, its happening. Everybody stay calm!^Michael Scott, The Office (US)",
-    "Oh hello, Jackie.^Jim Bell, Friday Night Dinner",
-    "I'm gonna wreck it!^Ralph, Wreck-it Ralph",
-    "Jolly good show!^Pops, Regular Show",
-    "Hamboninggggggg.^Rigby, Regular Show",
-    "You were the chosen one!^Obi-Wan Kenobi, Star Wars",
-    "ARE YOU WILLING TO FIGHT?!^Parzival, Ready Player One",
-    "Going outside is highly overrated.^Anorak, Ready Player One",
-    "Dude, am I in the frame?^Soos, Gravity Falls",
-    "Bacon Pancakes, making Bacon Pancakes!^Jake, Adventure Time",
-    "This is the way.^The Mandalorian",
-    "YUMYAN OWNS YOU ALL!^Yumyan, Kipo and the age of Wonderbeasts",
-    "Here's to another lousy millennium 🍻^ Phillip J Fry, Futurama"
+    "Ooohhhhhhhhh!!!!!\nRegular Show",
+    "I'm Pickle Rickkk!!!!\nRick, Rick and Morty",
+    "Bazinga!\nSheldon Cooper, The Big Bang Theory",
+    "I am the danger, I am the one who knocks.\nWalter White, Breaking Bad",
+    "Nobody exists on purpose.\nMorty, Rick and Morty",
+    "Bird is the word.\nPeter Griffin, Family Guy",
+    "You shall not pass!\nThe Lord of the Rings",
+    "Wubalubadubdub!\nRick, Rick and Morty",
+    "D'oh!\nHomer Simpson, The Simpsons",
+    "Cool cool cool cool cool cool cool cool.\nJake Peralta, Brooklyn Nine-Nine",
+    "We're all goofy goobers.\nSpongebob, Spongebob Square Pants",
+    "That's what she said.\nMichael Scott, The Office (US)",
+    "Oh my God, they killed Kenny!\nSouth Park",
+    "Oh my God, its happening. Everybody stay calm!\nMichael Scott, The Office (US)",
+    "Oh hello, Jackie.\nJim Bell, Friday Night Dinner",
+    "I'm gonna wreck it!\nRalph, Wreck-it Ralph",
+    "Jolly good show!\nPops, Regular Show",
+    "Hamboninggggggg.\nRigby, Regular Show",
+    "You were the chosen one!\nObi-Wan Kenobi, Star Wars",
+    "ARE YOU WILLING TO FIGHT?!\nParzival, Ready Player One",
+    "Going outside is highly overrated.\nAnorak, Ready Player One",
+    "Dude, am I in the frame?\nSoos, Gravity Falls",
+    "Bacon Pancakes, making Bacon Pancakes!\nJake, Adventure Time",
+    "This is the way.\nThe Mandalorian",
+    "YUMYAN OWNS YOU ALL!\nYumyan, Kipo and the age of Wonderbeasts",
+    "Here's to another lousy millennium 🍻\n Phillip J Fry, Futurama"
 );
 
 $ran = array_rand($quotes);
 $single_quote = $quotes[$ran];
-
-$arr2 = str_split($single_quote, '^')[0];
-$arr3 = str_split($single_quote, '^')[0];
 
 ?>
 
@@ -49,6 +46,6 @@ $arr3 = str_split($single_quote, '^')[0];
 
     <!-- Text -->
     <rect x="0" y="0" width="500" height="250" fill="black" rx="10"/>
-    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="white"><?php echo $arr2; ?></text>  
+    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="white"><?php echo $single_quote; ?></text>  
 
 </svg>
