eeaa9c26696afe1f252387b03c23770c17e92bde
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-04-28 19:41:52 +0100

    Update home.php

--- views/home.php
+++ views/home.php
@@ -8,7 +8,7 @@
   <title>TV/Movie Quotes</title>
 </head>
 <body>
-  <centre><h1>Go to /quote to generate a quote</h2></centre>
-  <centre><p>Developed by <a href="https://github.com/crxssed7">crxssed</a>, forked from <a href="https://github.com/pizidavi/Trakt-Widgets">Trakt Widgets</a></p></centre>
+  <center><h1>Go to /quote to generate a quote</h2></center>
+  <center><p>Developed by <a href="https://github.com/crxssed7">crxssed</a>, forked from <a href="https://github.com/pizidavi/Trakt-Widgets">Trakt Widgets</a></p></center>
 </body>
 </html>
