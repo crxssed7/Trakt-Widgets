64f4b35f6089112592688147cd8523f80cd12260
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-04-28 18:39:30 +0100

    Update home_new.php

--- views/home_new.php
+++ views/home_new.php
@@ -1,4 +1,5 @@
 <?php
+
 $quotes = array (
     "Ooohhhhhhhhh!!!!!"
     "I'm Pickle Rickkk!!!!"
@@ -32,14 +33,42 @@ $quotes = array (
 
 $ran = array_rand($quotes);
 $single_quote = $quotes[$ran];
+
 ?>
 
-<svg id="tv-quote-parent" width="720" height="198" viewBox="0 0 720 198" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <g id="text">
-        <rect x="0" y="74%" width="100%" height="26%" fill="#1c1c1c" />
+<svg id="poster_watched" width="720" height="198" viewBox="0 0 720 198" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <style>
+    <?php require 'fonts/Exo-2.php'; ?>
+    text,
+    tspan {
+      font-family: 'Exo 2', sans-serif;
+    }
+    tspan {
+      fill: #bfbfbf;
+      font-size: 80%;
+    }
+    </style>
+  </defs>
 
-        <rect x="0" y="74%" width="7%" height="26%" fill="#737373" />
+  <!-- Sfondo -->
+  <rect id="sfondo" x="0" y="0" width="100%" height="100%" fill="#ffffff" />
 
-        <text x="9%" y="94.9%" fill="#e6e6e6" font-size="20" dominant-baseline="bottom" text-anchor="left"><?php echo $single_quote; ?></text>
-    </g>
+  <!-- Image -->
+  <g id="image">
+    <rect x="0" y="0" width="100%" height="74%" fill="#f2f2f2" />
+    <image x="0" y="0" width="100%" xlink:href="" />
+  </g>
+
+  <!-- Text -->
+  <g id="text">
+    <rect x="0" y="74%" width="100%" height="26%" fill="#1c1c1c" />
+
+    <rect x="0" y="74%" width="7%" height="26%" fill="#737373" />
+    <image x="0.6%" y="76.5%" width="5.8%" xlink:href="" />
+
+    <text x="9%" y="83.9%" fill="#bfbfbf" font-size="14" dominant-baseline="bottom" text-anchor="left"></text>
+    <text x="9%" y="94.9%" fill="#e6e6e6" font-size="20" dominant-baseline="bottom" text-anchor="left"><?php echo $single_quote; ?></text>
+  </g>
+
 </svg>
